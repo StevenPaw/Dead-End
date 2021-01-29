66b811478107852653e38bb655eda4cc31e91531
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-01-29 16:40:37 +0100

    [ADDED] Models and Videotest

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void onStart()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+
+    public void onQuit()
+    {
+        Application.Quit();
+        Debug.Log("Application is now quitting");
+    }
+}
